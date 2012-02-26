f6fff0c007ecb4e5750238fa2b06d284c6a8a4cf
ptuna <ptunick@mail.umw.edu>
2012-02-26 15:19:53 -0500

    my attempt at making the visual login screen

--- pp_login.php
+++ pp_login.php
@@ -1,4 +1,32 @@
-<html>
+<?php if(!isset($_SESSION)){session_start();}  ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Login</title>
+</head>
+	<body onload="validate()">
+		<div id = "wrap">
+		<?php include("pp_header.html"); ?>
+			<?php if(isset($errorMessage)){echo '<br><font color="red"><b>Error - the following entries need to be fixed:</b><ul>',$errorMessage,'</ul></font><br>',"\n";}?>
+			<form name="login" method ="post" action="pp_login_controller.php" onsubmit="document.register.submit.disabled=true;document.register.submit.value='Processing...'">
+			<table align=center>
+				<tr>
+					<td>Username:</td>
+					<td><input type="text" id="username" name="username" </td>
+				</tr>
+				<tr>
+					<td>Password:</td>
+					<td><input type="password" id="password" name="password" </td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="submit" value="Login"></td>
+				</tr>
+			</table>
+			</form>
+		</div>
+	</body>
+</html>
 
 <body>
 
