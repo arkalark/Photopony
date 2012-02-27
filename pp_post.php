d0d7b83366a9bc38caa825ede5e5bf53378554ea
Jacob Nedza <jnedza@mail.umw.edu>
2012-02-27 18:55:02 -0500

    Basic post and post controller. Needs detailing, otherwise functional

--- pp_post.php
+++ pp_post.php
@@ -1,3 +1,8 @@
+
+	<?php
+	include('pp_db.php');
+	?>
+
 <html>
 
 	<?php
@@ -15,6 +20,22 @@
 			
 		*/
 	
+	
+	
 	?>
 
+	<form method="post" action="pp_post_controller.php">
+	<label for="username">Post a New Thread</label><br>
+	Choose a board, enter a thread title, and enter some content!<br>
+	Picture Link: <input type="text" id="link" name="link" /><br>
+	<select name="board">
+		<option value="art">Art</option>
+	</select>
+    <input type="text" id="title" name="title" /><br>
+	<textarea name="content" cols="40" rows="5">
+	Enter your post here!
+	</textarea><br>
+	<input type="submit" value="Post!" name="submit" />
+	</form>
+	
 </html>
--- pp_post_controller.php
+++ pp_post_controller.php
@@ -1,6 +1,26 @@
 
 	<?php
 	
+	include('pp_db.php');
 	
+		/*
+
+		$_POST['content']
+		$_POST['title']
+		$_POST['board']
+		$_POST['link']
+		
+		*/
+		$content = $_POST['content'];
+		$title = $_POST['title'];
+		$board = $_POST['board'];
+		$link = $_POST['link'];
+		
+		//echo("Checking: $_POST[content]");
+		
+		$query = "INSERT INTO threads (id, name, piclink, content, board) VALUES (NULL, '$title', '$link', '$content', '$board');";
+		echo("$query");
+		
+		mysqli_query($db, $query) or die("Error Querying Database");
 	
 	?>
